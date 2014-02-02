88ada2c707e0aa80ffd9b5b22a857b334ba2a058
dastardlylemon <jwu@efunda.com>
2014-02-02 02:52:52 -0800

    implement option switching

--- js/owaranai.js
+++ js/owaranai.js
@@ -24,19 +24,55 @@ function startTime() {
 	t = setTimeout(function(){startTime()},500);
 }
 
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var defaultRanking = "Daily";
 
-$(document).ready(function() {
-	startTime();
+function loadOptions() {
+	var ranking = localStorage["rankingType"];
+	if (ranking == undefined) {
+		ranking = defaultRanking;
+	}
+	
+	$('input[name=pgata]:radio').each(function(i, d) {
+		if (d['value'] == ranking) {
+			d.checked = true;
+		}
+	});
+}
 
-  var d = new Date();
-  $('#date').text(days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate());
-  $('#time').fadeIn(1000);
-  $('#date').fadeIn(1000);
-  
-  var timestamp = d.getFullYear().toString() + getMonth(d) + getDate(d);
-  var json = 'http://cdn-pixiv.lolita.tw/rankings/' + timestamp + '/pixiv_daily.json';
+function saveOptions() {
+	$('input[name=pgata]:radio').each(function(i, d) {
+		if (d.checked == true) {
+			localStorage["rankingType"] = d['value'];
+		}
+	});
+}
+
+function loadImages(ranking) {
+	var rurl;
+	switch(ranking) {
+		case 'Daily':
+			rurl = '/pixiv_daily.json';
+		break;
+		case 'Weekly':
+			rurl = '/pixiv_weekly.json';
+		break;
+		case 'Monthly':
+			rurl = '/pixiv_monthly.json';
+		break;
+		case 'Rookie':
+			rurl = '/pixiv_rookie.json';
+		break;
+		case 'Original':
+			rurl = '/pixiv_original.json';
+		break;
+		default: 
+			rurl = '/pixiv_daily.json';
+		break;
+	}
+
+	var d = new Date();
+	var timestamp = d.getFullYear().toString() + getMonth(d) + getDate(d);
+  var json = 'http://cdn-pixiv.lolita.tw/rankings/' + timestamp + rurl;
 
   var items = [];
   var req = $.getJSON(json, function(response) {
@@ -48,6 +84,11 @@ $(document).ready(function() {
       }
     });
   });
+  
+  $('#loader').fadeIn(400);
+  $('#content').fadeOut(400);
+  $('#content').empty();
+  $('#content').fadeIn(400);
   
   req.complete(function() {
   	$("<div/>", {"id": "img-list", html: items.join("")}).appendTo("#content").each(function() {
@@ -62,5 +103,35 @@ $(document).ready(function() {
 				});
 	  	});
 	  });
+  });
+}
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+$(document).ready(function() {
+	startTime();
+	loadOptions();
+
+  var d = new Date();
+  $('#date').text(days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate());
+  $('#time').fadeIn(1000);
+  $('#date').fadeIn(1000);
+  
+  loadImages(localStorage["rankingType"]);
+  
+  $('#settings').click(function() {
+  	$('#sp-wrapper').fadeIn(400);
+  	$('#blackout').fadeIn(400);
+  });
+  
+  $('#dismiss').click(function() {
+  	$('#sp-wrapper').fadeOut(400);
+  	$('#blackout').fadeOut(400);
+  });
+  
+  $('input[type="radio"]').click(function(){
+    saveOptions();
+    loadImages(localStorage["rankingType"]);
   });
 });
