ba296572ae555438eaaa89b10f58c671d2010628
James Wu <jwu@efunda.com>
2014-03-22 00:47:08 -0700

    switched to coffeescript

---
+++ js/old/owaranai-bak.js
@@ -0,0 +1,182 @@
+function randomize(arr) {
+  for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+    return arr;
+};
+
+function formatTime(i) {
+  return i < 10 ? '0' + i : i;
+}
+
+function getMonth(d) {
+  var month = d.getMonth() + 1;
+  return formatTime(month);
+}
+
+function getDate(d) {
+  var date = d.getDate();
+  return formatTime(date);
+}
+
+function truncate(string) {
+  if (string.length > 30)
+    return string.substring(0,30)+'...';
+  else
+    return string;
+};
+
+function startTime() {
+  var d = new Date();
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+  h = formatTime(h);
+  m = formatTime(m);
+  s = formatTime(s);
+  $('#time').text(h + " " + m + " " + s);
+  t = setTimeout(function(){startTime()},500);
+}
+
+var defaultRanking = "Daily";
+
+function loadOptions() {
+  var ranking = localStorage["rankingType"];
+  if (ranking == undefined) {
+    ranking = defaultRanking;
+  }
+
+  $('input[name=pgata]:radio').each(function(i, d) {
+    if (d['value'] == ranking) {
+      d.checked = true;
+    }
+  });
+}
+
+function saveOptions() {
+  $('input[name=pgata]:radio').each(function(i, d) {
+    if (d.checked == true) {
+      localStorage["rankingType"] = d['value'];
+    }
+  });
+}
+
+function loadImages(ranking) {
+  var rurl;
+  switch(ranking) {
+    case 'Daily':
+      rurl = '/pixiv_daily.json';
+    break;
+    case 'Weekly':
+      rurl = '/pixiv_weekly.json';
+    break;
+    case 'Monthly':
+      rurl = '/pixiv_monthly.json';
+    break;
+    case 'Rookie':
+      rurl = '/pixiv_rookie.json';
+    break;
+    case 'Original':
+      rurl = '/pixiv_original.json';
+    break;
+    default:
+      rurl = '/pixiv_daily.json';
+    break;
+  }
+
+  var d = new Date();
+  var timestamp = d.getFullYear().toString() + getMonth(d) + getDate(d);
+  var json = 'http://cdn-pixiv.lolita.tw/rankings/' + timestamp + rurl;
+
+  var items = [];
+  var req = $.getJSON(json, function(response) {
+    var i = 0;
+    randomize(response);
+    $.each(response, function(key, val) {
+      if (i < 30) {
+        items.push("<div style='display: none' class='pxvimg'><a href='" + val['url'] + "'><img src='" + val['img_url'] + "'></a></div>");
+        i++;
+      }
+    });
+  });
+
+  $('#loader').fadeIn(400);
+  $('#content').fadeOut(400, function() {
+    $('#content').empty();
+    $('#content').show();
+
+    req.complete(function() {
+      $("<div/>", {"id": "img-list", html: items.join("")}).appendTo("#content").each(function() {
+        $('#img-list').waitForImages(function() {
+          $('#loader').fadeOut(400);
+          $('#img-list').isotope({
+            itemSelector : '.pxvimg',
+          });
+
+          $('.pxvimg').each(function(index) {
+            $(this).delay(100*index).fadeIn(400);
+          });
+        });
+      });
+    });
+  });
+}
+
+function populateTabList() {
+  var tabs = [];
+  chrome.sessions.getRecentlyClosed(function(sessions) {
+    $('#nodev').hide();
+    var c = 0;
+    $.each(sessions, function(i, v) {
+      if (v.tab != null && v.tab.url.substring(0,9) != "chrome://" && c < 10) {
+        tabs.push("<div class='rc-link'><a href='" + v.tab.url + "'>" + truncate(v.tab.title) + "</a></div>");
+        c++;
+      }
+    });
+    $("<div/>", {"id": "rc-list", html: tabs.join("")}).appendTo("#rc-panel");
+  });
+}
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+$(document).ready(function() {
+  startTime();
+  loadOptions();
+
+  var d = new Date();
+  $('#date').text(days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate());
+
+  loadImages(localStorage["rankingType"]);
+
+  $('#settings').click(function() {
+    $('#sp-wrapper').fadeIn(400);
+    $('#blackout').fadeIn(400);
+  });
+
+  $('#dismiss').click(function() {
+    $('#sp-wrapper').fadeOut(400);
+    $('#blackout').fadeOut(400);
+  });
+
+  var debounce;
+
+  $('#rc-button, #rc-panel').mouseenter(function() {
+    $('#rc-panel').fadeIn(400);
+    clearTimeout(debounce);
+  });
+
+  $('#rc-panel, #rc-button').mouseleave(function() {
+    debounce = setTimeout(closeMenu, 400);
+  });
+
+  var closeMenu = function() {
+    $('#rc-panel').fadeOut(400);
+    clearTimeout(debounce);
+  }
+
+  $('input[type="radio"]').click(function(){
+    saveOptions();
+    loadImages(localStorage["rankingType"]);
+  });
+
+  populateTabList();
+});
---
+++ js/owaranai.coffee
@@ -0,0 +1,129 @@
+randomize = (arr) ->
+  curIndex = arr.length
+
+  while 0 != curIndex
+    randIndex = Math.floor Math.random() * curIndex
+    curIndex -= 1;
+    tempVal = arr[curIndex]
+    arr[curIndex] = arr[randIndex]
+    arr[randIndex] = tempVal
+
+formatTime = (time) ->
+  if time < 10 then '0' + time else time
+
+getDate = (d) -> formatTime d.getDate()
+
+getMonth = (d) -> formatTime d.getMonth() + 1
+
+truncate = (string) ->
+  if string.length > 30 then string.substring(0,30) + '...' else string
+
+startTime = ->
+  d = new Date()
+  h = formatTime d.getHours()
+  m = formatTime d.getMinutes()
+  s = formatTime d.getSeconds()
+  $('#time').text h + ' ' + m + ' ' + s
+  setTimeout (-> startTime()), 500
+
+defaultRanking = 'Daily'
+
+loadOptions = ->
+  ranking = localStorage['rankingType']
+  if not ranking? then ranking = defaultRanking
+  console.log ranking
+  $('input[name=pgata]:radio').each ->
+    if $(@)[0].value == ranking then $(@)[0].checked = true
+
+saveOptions = ->
+  $('input[name=pgata]:radio').each ->
+    if $(@)[0].checked then localStorage['rankingType'] = $(@)[0].value
+
+loadImages = (ranking) ->
+  switch ranking
+    when 'Daily' then rurl = '/pixiv_daily.json'
+    when 'Weekly' then rurl = '/pixiv_weekly.json'
+    when 'Monthly' then rurl = '/pixiv_monthly.json'
+    when 'Rookie' then rurl = '/pixiv_rookie.json'
+    when 'Original' then rurl = '/pixiv_original.json'
+    else rurl = '/pixiv_daily.json'
+
+  d = new Date()
+  timestamp = d.getFullYear().toString() + getMonth(d) + getDate(d)
+  json = 'http://cdn-pixiv.lolita.tw/rankings/' + timestamp + rurl
+
+  items = []
+  req = $.getJSON json, (response) ->
+    i = 0
+    randomize(response)
+    while i < 30
+      val = response[i]
+      items.push("<div style='display: none' class='pxvimg'><a href='" + val['url'] + "'><img src='" + val['img_url'] + "'></a></div>")
+      i++
+
+  $('#loader').fadeIn 400
+  $('#content').fadeOut 400, ->
+    $('#content').empty()
+    $('#content').show()
+
+    req.complete ->
+      d = $('<div/>', {'id': 'img-list', 'html': items.join('')})
+      d.appendTo('#content').each ->
+        $('#img-list').waitForImages ->
+          $('#loader').fadeOut 400
+          $('#img-list').isotope {'itemSelector' : '.pxvimg'}
+          $('.pxvimg').each (index) ->
+            $(@).delay(100 * index).fadeIn 400
+
+populateTabList = ->
+  chrome.sessions.getRecentlyClosed (sessions) ->
+    $('#nodev').hide()
+    tabs = for session in sessions
+      if session.tab? and session.tab.url.substring(0,9) != 'chrome://' and _results.length < 10
+      then "<div class='rc-link'><a href='" + session.tab.url + "'>" + truncate(session.tab.title) + "</a></div>"
+
+    $('<div/>', {'id': 'rc-list', 'html': tabs.join('')}).appendTo '#rc-panel'
+
+days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+months = [
+  'January', 'February', 'March'
+  'April', 'May', 'June'
+  'July', 'August', 'September'
+  'October', 'November', 'December'
+]
+
+$(document).ready ->
+  startTime()
+  loadOptions()
+
+  d = new Date()
+  $('#date').text days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate()
+
+  loadImages localStorage['rankingType']
+
+  debounce = null
+
+  $('#settings').click ->
+    $('#sp-wrapper').fadeIn 400
+    $('#blackout').fadeIn 400
+
+  $('#dismiss').click ->
+    $('#sp-wrapper').fadeOut 400
+    $('#blackout').fadeOut 400
+
+  $('#rc-panel, #rc-button').mouseleave ->
+    debounce = setTimeout closeMenu, 400
+
+  $('#rc-button, #rc-panel').mouseenter ->
+    $('#rc-panel').fadeIn 400
+    clearTimeout debounce
+
+  closeMenu = ->
+    $('#rc-panel').fadeOut 400
+    clearTimeout debounce
+
+  $('input[type="radio"]').click ->
+    saveOptions()
+    loadImages localStorage['rankingType']
+
+  populateTabList()
